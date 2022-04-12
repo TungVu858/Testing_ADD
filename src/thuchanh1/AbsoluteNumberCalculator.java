3afb4d98e010c958f316991e99aed59b741bd98e
TungVu858 <tungvt858@gmail.com>
2022-04-12 11:48:14 +0700

    done

---
+++ src/thuchanh1/AbsoluteNumberCalculator.java
@@ -0,0 +1,12 @@
+package thuchanh1;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number){
+        if (number<0){
+            return -number;
+        }
+        else {
+            return number;
+        }
+    }
+}
